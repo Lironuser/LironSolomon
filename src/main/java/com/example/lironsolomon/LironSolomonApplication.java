72e2f490615c79a22ab5a31c8c8f7e427503346a
Liron <lironuser@gmail.com>
2023-01-16 21:18:17 +0200

    jkjl

---
+++ src/main/java/com/example/lironsolomon/LironSolomonApplication.java
@@ -0,0 +1,13 @@
+package com.example.lironsolomon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LironSolomonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LironSolomonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lironsolomon/LironSolomonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lironsolomon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LironSolomonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
