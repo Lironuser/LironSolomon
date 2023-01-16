ebbab4c1f6f6973444f508cd990613c0d953df4e
Liron <lironuser@gmail.com>
2023-01-16 23:28:37 +0200

    1.16.23 11:30 - add - insert and delete

--- src/main/java/com/example/lironsolomon/Connect.java
+++ src/main/java/com/example/lironsolomon/Connect.java
@@ -8,7 +8,7 @@ public class Connect {
         String password = "Liron1630";
         try {
             Connection connection = DriverManager.getConnection(jdbcURL, userName, password);
-            System.out.printf("There is connection");
+            System.out.printf("\nConnection complete\n");
 
             return connection;
         } catch (SQLException e) {
@@ -18,15 +18,38 @@ public class Connect {
         }
 
     }
-    public void viewTable(Connection con) throws SQLException  {
-        String query = "select * from data";
+    public void viewTable(Connection con) throws RuntimeException {
+        String query = "select * from Ishambel";
         try (Statement stmt = con.createStatement()) {
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
-                String dd = rs.getString("dd");
+                String text = rs.getString("text");
                 int ID = rs.getInt("id");
-                System.out.println(dd + ", " +ID);
+                System.out.println(text + ", " +ID);
             }
+            con.close();
+        } catch (SQLException e) {
+            System.out.println("Error!!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void Insert(Connection con,String text)  throws SQLException{
+        String Text=text;
+        String query = "insert into Ishambel (Text)\n" +
+                "VALUES ('"+text+"');";
+        try (Statement stmt = con.createStatement()) {
+            int rs = stmt.executeUpdate(query);
+            con.close();
+        } catch (SQLException e) {
+            System.out.printf("Error!!");
+            throw new RuntimeException(e);
+        }
+    }
+    public void Delete(Connection con, String text1) throws SQLException{
+        String query = "delete from Ishambel where text = 'Liron'";
+        try (Statement stmt = con.createStatement()) {
+            int rs = stmt.executeUpdate(query);
             con.close();
         } catch (SQLException e) {
             System.out.printf("Error!!");
--- src/main/java/com/example/lironsolomon/LironSolomonApplication.java
+++ src/main/java/com/example/lironsolomon/LironSolomonApplication.java
@@ -3,12 +3,18 @@ package com.example.lironsolomon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class LironSolomonApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LironSolomonApplication.class, args);
-        System.out.println("hey");
-
+    public static void main(String[] args) throws SQLException {
+        //SpringApplication.run(LironSolomonApplication.class, args);
+        System.out.println("Welcome");
+        Connect con = new Connect();
+        con.connect();
+        con.Insert(con.connect(),"Liron");
+        con.Delete(con.connect(),"Liron");
+        con.viewTable(con.connect());
     }
 }
