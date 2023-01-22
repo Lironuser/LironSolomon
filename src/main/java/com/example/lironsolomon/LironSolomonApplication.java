851f8fdb454511589eb0b50285b6548bdbd752c9
Liron <lironuser@gmail.com>
2023-01-22 12:53:50 +0200

    22/01/2023 - 12:20

--- src/main/java/com/example/lironsolomon/LironSolomonApplication.java
+++ src/main/java/com/example/lironsolomon/LironSolomonApplication.java
@@ -1,6 +1,5 @@
 package com.example.lironsolomon;
 import javax.swing.JOptionPane;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.SQLException;
@@ -14,7 +13,7 @@ public class LironSolomonApplication {
             JOptionPane.showMessageDialog(null, "Welcome to - Liron Solomon project.");
             Connect con = new Connect();
             con.connect();
-            con.Insert(con.connect(), "Liron");
+            //con.Insert(con.connect(), "Liron");
             //con.Delete(con.connect(), 47);
             con.SelectAll(con.connect());
         }else{
